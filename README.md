923e03afc9b5855aad7ceb1d47a0ff27395d0dd6
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-08-06 09:40:53 +0200

    add: implement counter functionality with increment, decrement, and reset

---
+++ README.md
@@ -0,0 +1,15 @@
+# ESERCIZIO 1: Contatore con useReducer
+Obiettivo: creare un contatore che può essere incrementato, decrementato e resettato.
+
+Requisiti:
++ aumenta il conteggio
+
+- diminuisce il conteggio
+
+Reset lo riporta a 0
+
+Suggerimenti:
+Stato iniziale: { count: 0 }
+
+Azioni: 'increment', 'decrement', 'reset'
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react"
 
 
 function App() {
 
+  // variabile di stato per gestire il contatore 
+  const [count, setCount] = useState(0);
+
+  // funzione per incrementare i numeri 
+  function increaseNumber() {
+    setCount(prev => prev + 1);
+  }
+
+  // funzione per diminuire i numeri 
+  function decreaseNumber() {
+    setCount(prev => prev - 1);
+  }
+
+  // funzione per resettare i numeri 
+  function resetNumber() {
+    setCount(0);
+  }
 
   return (
     <>
-      Sono in App
+      <p>{count}</p>
+      <button onClick={increaseNumber}>Aumenta</button>
+      <button onClick={decreaseNumber} disabled={count === 0}>Diminuisci</button>
+      <button onClick={resetNumber}>Reset</button>
     </>
   )
 }
